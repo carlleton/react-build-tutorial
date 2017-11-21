259cc3f7853a03d2852f4348fc2bbd70137f25fb
carlleton <carlleton@hotmail.com>
2017-11-21 11:36:33 +0800

    1

---
+++ demo/src/components/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import styles from './app.less'
+
+class App extends Component {
+  render () {
+    return (
+      <h1 className={styles.title}>hello</h1>
+    )
+  }
+}
+
+export default App
